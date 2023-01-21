a4e56ca8a1f37a9f98fd5ffd0ade887e84cc1403
kevin-lomenen <kevin.lomenen@student.moringaschool.com>
2023-01-21 16:14:55 +0300

    done

---
+++ src/components/Introduction.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SearchForm from "./SearchForm";
+
+export default function Introduction() {
+  return (
+    <div className="introduction">
+      <h1 className="introduction-title">Knight Travel Agencies </h1>
+      <h2 className="introduction-comment">Enjoy our best travel exprience</h2>
+      <SearchForm />
+    </div>
+  );
+}
---
+++ src/components/SearchForm.js
@@ -0,0 +1,158 @@
+/* eslint-disable array-callback-return */
+import React, { useState } from "react";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
+import { RiBuilding2Line } from 'react-icons/ri';
+
+// import CitiesData from "./cities.json";
+export default function SearchForm() {
+  let history = useNavigate();
+  const [formData, setFormData] = useState({
+    from: "",
+    to: "",
+    date: moment().format("YYYY-MM-DD"),
+  });
+
+  const [cityData,] = useState([]);
+
+  // useEffect(() => {
+  //   setCityData(getCity());
+  // }, []);
+
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    // eslint-disable-next-line eqeqeq
+    if (formData.from != "" && formData.to != "" && formData.date != "") {
+      history.push(
+        `/searchbus?from=${formData.from}&to=${formData.to}&date=${formData.date}`
+      );
+    }
+    event.preventDefault();
+  }
+
+  function handleToButton(event) {
+    setFormData({
+      ...formData,
+      "to": event.target.id,
+    });
+    event.preventDefault();
+  }
+
+  function handleFromButton(event) {
+    setFormData({
+      ...formData,
+      "from": event.target.id,
+    });
+    event.preventDefault();
+  }
+
+  function capitalize(word) {
+    const lower = word.toLowerCase();
+    return word.charAt(0).toUpperCase() + lower.slice(1);
+  }
+
+  // function getCity() {
+  //   let city = JSON.parse(JSON.stringify(CitiesData));
+
+  //   let cities = city.map((each) => each.city);
+
+  //   return cities;
+  // }
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-each-box">
+          <RiBuilding2Line/>
+          <input
+            name="from"
+            type="text"
+            placeholder="FROM"
+            value={capitalize(formData.from)}
+            onChange={handleChange}
+            className="form-input"
+            required={true}
+          ></input>
+          {formData.from === "" ? (
+            <div></div>
+          ) : (
+            <div className="form-input-select-box">
+              {cityData.map((each) => {
+                if (
+                  formData.from !== each &&
+                  formData.from === each.slice(0, formData.from.length)
+                ) {
+                  return (
+                    <button
+                      id={each}
+                      onClick={handleFromButton}
+                      className="form-input-select"
+                    >
+                      {each}
+                    </button>
+                  );
+                }
+              })}
+            </div>
+          )}
+        </div>
+        <div className="form-each-box">
+        <RiBuilding2Line/>
+          <input
+            name="to"
+            type="text"
+            placeholder="TO"
+            value={capitalize(formData.to)}
+            onChange={handleChange}
+            className="form-input"
+            required={true}
+          ></input>
+          {formData.to === "" ? (
+            <div></div>
+          ) : (
+            <div className="form-input-select-box">
+              {cityData.map((each) => {
+                if (
+                  formData.to !== each &&
+                  formData.to === each.slice(0, formData.to.length)
+                ) {
+                  return (
+                    <button
+                      id={each}
+                      onClick={handleToButton}
+                      className="form-input-select"
+                    >
+                      {each}
+                    </button>
+                  );
+                }
+
+                return <></>;
+              })}
+            </div>
+          )}
+        </div>
+        <div className="form-each-box">
+        <RiBuilding2Line/>
+          <input
+            name="date"
+            type="date"
+            value={formData.date}
+            onChange={handleChange}
+            min={moment().format("YYYY-MM-DD")}
+            className="form-input"
+          ></input>
+        </div>
+        <button type="submit" className="form-submit-button">
+          Search Buses
+        </button>
+      </form>
+    </>
+  );
+}
