f842af134eff501298f638c918c96c44dcd5232c
kevin-lomenen <kevin.lomenen@student.moringaschool.com>
2023-01-21 18:41:52 +0300

    added booking options and passenger details

---
+++ src/components/BookingOptions.js
@@ -0,0 +1,263 @@
+import moment from "moment";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  selectSeats,
+} from "../features/seats/seatsSlice";
+import PassengerDetail from "./PassengerDetail";
+
+export default function BookingOptions({ data }) {
+  const seats = useSelector(selectSeats);
+
+  const [selectedOption, setSelectedOption] = useState("booking");
+  const [selectedData, setSelectedData] = useState([]);
+  const [boardingPoint, setBoardingPoint] = useState("");
+  const [droppingPoint, setDroppingPoint] = useState("");
+  const [showBill, setShowBill] = useState(false);
+  const [showPassengerDetail, setShowPassengerDetail] = useState(false);
+  const [missingData, setMissingData] = useState("");
+
+  const ButtonRef = useRef();
+
+  let {
+    boarding_point,
+    dropping_point,
+    timing,
+    share_seat_price,
+    single_seat_price,
+  } = data;
+
+  useEffect(() => {
+    if (selectedOption === "booking") {
+      setSelectedData(boarding_point);
+    } else {
+      setSelectedData(dropping_point);
+    }
+  }, [boarding_point, dropping_point, selectedOption]);
+
+  function handleCheckboxClick(id) {
+    console.log(`clicked ${id}`);
+    if (selectedOption === "booking") {
+      setBoardingPoint(id);
+    } else {
+      setDroppingPoint(id);
+    }
+  }
+
+  function handleContinueClick() {
+    if (totalCost() === 0) {
+      setMissingData("Select Seat/Seats");
+      console.log("select seatss");
+    } else if (boardingPoint === "") {
+      setMissingData("Select Boarding Point");
+      console.log("select boarding point");
+    } else if (droppingPoint === "") {
+      setMissingData("Select Dropping Point");
+      console.log("select Dropping Point");
+    }
+
+    if (totalCost() !==0) {
+      if (boardingPoint !== "" && droppingPoint !== "") {
+        setShowBill(!showBill);
+      }
+    }
+  }
+
+  function handleProceedClick() {
+    if (totalCost() !== 0) {
+      setShowPassengerDetail(true);
+    }
+  }
+
+  function handleCloseClick() {
+    setShowPassengerDetail(false);
+  }
+
+  function totalCost() {
+    let value = seats.map((each) => {
+      if (each.length === 2) {
+        return share_seat_price;
+      } else {
+        return single_seat_price;
+      }
+    });
+
+    if (value.length !== 0) {
+      return value.reduce((total, num) => total + num);
+    }
+    return 0;
+  }
+
+  if (showBill) {
+    let boardingData = boarding_point.find(
+      (each) => each._id === boardingPoint
+    );
+
+    let droppingData = dropping_point.find(
+      (each) => each._id === droppingPoint
+    );
+
+    console.log(boardingData.address);
+    return (
+      <>
+        {showPassengerDetail ? (
+          <PassengerDetail
+            handleCloseClick={handleCloseClick}
+            total={totalCost()}
+            timing={timing}
+            boardingData={boardingData}
+            droppingData={droppingData}
+          />
+        ) : (
+          <></>
+        )}
+        <div className="bill">
+          <div className="bill-data">
+            <div className="bill-data-each">
+              <strong>Boarding & Dropping</strong>
+              <button
+                className="bill-data-button"
+                onClick={handleContinueClick}
+              >
+                Change
+              </button>
+            </div>
+            <div className="bill-data-style">
+              <div className="bill-data-each">
+                <span>{boardingData.address}</span>
+                <span style={{ fontWeight: "bold" }}>
+                  {moment(timing.arrival)
+                    .add(boardingData.add_time, "h")
+                    .format("HH:mm")}
+                </span>
+              </div>
+              <div className="bill-data-each">
+                <span>{droppingData.address}</span>
+                <span style={{ fontWeight: "bold" }}>
+                  {moment(timing.departure)
+                    .add(droppingData.add_time, "h")
+                    .format("HH:mm")}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="bill-data-each bill-data-seats ">
+            <strong>Seat No.</strong>
+            <div>
+              {seats.map((each) => {
+                return (
+                  <span key={each} className="bill-data-seat">
+                    {each}
+                    {seats.length > 1 ? "," : ""}
+                  </span>
+                );
+              })}
+            </div>
+          </div>
+          <div className="bill-data-each">
+            <strong>Fare Detail</strong>
+          </div>
+          <div className="bill-data-each">
+            <div>
+              <strong>Amount</strong>
+              <div>
+                <span className="bill-data-subtitle">
+                  (Taxes will be calculated during payment)
+                </span>
+              </div>
+            </div>
+            <strong>INR {totalCost()}</strong>
+          </div>
+          <div className="bill-data-each">
+            <button
+              className="booking-confirm-button"
+              onClick={handleProceedClick}
+            >
+              PROCEED TO BOOK
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="booking-option">
+        <div className="booking-option-button-box">
+          <button
+            className={
+              selectedOption === "booking"
+                ? "booking-option-button booking-option-button-selected"
+                : "booking-option-button"
+            }
+            onClick={() => setSelectedOption("booking")}
+          >
+            BOARDING POINT
+          </button>
+          <button
+            className={
+              selectedOption === "dropping"
+                ? "booking-option-button booking-option-button-selected"
+                : "booking-option-button"
+            }
+            onClick={() => setSelectedOption("dropping")}
+          >
+            DROPING POINT
+          </button>
+        </div>
+        <ul>
+          {selectedData &&
+            selectedData.map((each) => {
+              return (
+                <li
+                  key={each._id}
+                  ref={ButtonRef}
+                  name={each._id}
+                  className="booking-option-each-checkbox"
+                  onClick={() => {
+                    handleCheckboxClick(each._id);
+                  }}
+                >
+                  <button
+                    className={
+                      boardingPoint === each._id || droppingPoint === each._id
+                        ? "booking-option-each-checkbox-logo-select"
+                        : "booking-option-each-checkbox-logo-outer"
+                    }
+                  ></button>
+                  <span className="booking-option-each-time">
+                    {selectedOption === "booking"
+                      ? moment(timing.departure)
+                          .add(each.add_time, "m")
+                          .format("HH:mm")
+                      : moment(timing.arrival)
+                          .add(each.remove_time, "m")
+                          .format("HH:mm")}
+                  </span>
+                  <span className="booking-option-each-address">
+                    {each.address}
+                  </span>
+                </li>
+              );
+            })}
+        </ul>
+        <div className="booking-confirm">
+          <span className="booking-confirm-alert">{missingData}</span>
+          <div className="booking-confirm-button-box">
+            <strong>Amount</strong>
+            <strong>
+              INR <span>{totalCost()}</span>
+            </strong>
+          </div>
+          <button
+            className="booking-confirm-button"
+            onClick={handleContinueClick}
+          >
+            Continue
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/PassengerDetail.js
@@ -0,0 +1,192 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import EachPassengerForm from "./EachPassengerForm";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate} from "react-router-dom";
+import axios from "axios";
+
+import {
+  emptyAll,
+  selectBus,
+  selectSeats,
+  selectContactData,
+  selectPassengerData,
+  selectSelectedBusData,
+  selectDate,
+} from "../features/seats/seatsSlice";
+import { BsPersonCircle, SlEnvolopeLetter } from "react-icons/bs/sl";
+import ContactForm from "./ContactForm";
+import checked from "../images/checked.svg";
+import moment from "moment";
+import LoadingPayment from "./LoadingPayment";
+
+export default function PassengerDetail({
+  handleCloseClick,
+  total,
+  boardingData,
+  droppingData,
+  timing,
+}) {
+  const seats = useSelector(selectSeats);
+  const contactData = useSelector(selectContactData);
+  const passengerData = useSelector(selectPassengerData);
+  const busData = useSelector(selectSelectedBusData);
+  const date = useSelector(selectDate);
+
+  let busId = useSelector(selectBus);
+
+  const dispatch = useDispatch();
+  const history = useNavigate();
+
+  const [isSubmitForm, setIsSubmitForm] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (seats.length === passengerData.length && contactData.length !== 0) {
+      console.log("Save Data to Database");
+      bookTickets();
+    }
+    return () => {
+      setIsSubmitForm(false);
+    };
+  }, [isSubmitForm === true]);
+
+  function SubmitForm() {
+    setIsSubmitForm(true);
+    // if (seats.length === passengerData.length && contactData.length != 0) {
+    //   console.log("Save Data to Database");
+    //   bookTickets();
+    // }
+  }
+
+  function UnSubmitForm() {
+    setIsSubmitForm(false);
+  }
+
+  async function bookTickets() {
+    const requests = passengerData.map((each) =>
+      axios
+        .post("/bus/bookticket", {
+          id: busId,
+          data: {
+            name: each.name,
+            gender: each.gender,
+            age: parseInt(each.age),
+            email: "Duren124@gmail.com",
+            phone: 9405135957,
+            boarding_point: each.boarding_point,
+            dropping_point: each.dropping_point,
+            boarding_time: moment(timing.arrival)
+              .add(boardingData.add_time, "m")
+              .format("HH:mm A"),
+            dropping_time: moment(timing.departure)
+              .add(droppingData.remove_time, "m")
+              .format("HH:mm A"),
+            price: total,
+            date: moment(date).format("dddd, MMMM DD, YYYY"),
+            seatno: each.seatno,
+          },
+        })
+        .catch((err) => null)
+    );
+
+    try {
+      setIsLoading(true);
+      const responses = await axios.all(requests);
+      if (responses && responses.length !== 0) {
+        dispatch(emptyAll());
+        console.log(responses);
+        let ticketInfo = responses.map((each) => each.data);
+        history.push({
+          pathname: "/bookedtickets",
+          state: {
+            tickets: ticketInfo,
+            total: total,
+            data: busData,
+          },
+        });
+        setIsLoading(false);
+      }
+      console.log(responses);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  if (isLoading) {
+    return (
+      <div className="passenger">
+        <div className="payment-detail">
+          <div className="payment-loading-half-circle"></div>
+          <div className="payment-box">
+            <img src={checked} alt="payment-logo" />
+            <h3 className="payment-title">Payment Successful</h3>
+          </div>
+          <div className="payment-amount-box">
+            <span className="payment-amount-title">Amount Paid: </span>
+            <span className="payment-amount-value">Rs. {total} </span>
+          </div>
+          <div className="payment-pdf">
+            <LoadingPayment title={"Generating your PDF ticket"} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="passenger">
+      <div className="passenger-detail">
+        <div className="passenger-row">
+          <h3>Passenger Detail</h3>
+          <button
+            className="passenger-back-button"
+            onClick={handleCloseClick}
+          ></button>
+        </div>
+        <div className="passenger-scroll">
+          <div>
+            <div className="passenger-information-box">
+            < BsPersonCircle />
+              <h4>Passenger Information</h4>
+            </div>
+            {seats.map((id, index) => {
+              return (
+                <EachPassengerForm
+                  key={id}
+                  id={id}
+                  index={index}
+                  isSubmitForm={isSubmitForm}
+                  UnSubmitForm={UnSubmitForm}
+                  boardingData={boardingData}
+                  droppingData={droppingData}
+                />
+              );
+            })}
+          </div>
+          <div>
+            <div className="passenger-information-box">
+            <SlEnvolopeLetter/>
+              <h4>Contact Details</h4>
+            </div>
+            <ContactForm
+              isSubmitForm={isSubmitForm}
+              UnSubmitForm={UnSubmitForm}
+            />
+          </div>
+        </div>
+        <div className="passenger-bill">
+          <div className="passenger-bill-amount">
+            <span>Total Amount :</span>
+            <span>
+              <strong>INR {total}</strong>
+            </span>
+          </div>
+          <button className="passenger-book-ticket-button" onClick={SubmitForm}>
+            PROCEED TO PAY
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
