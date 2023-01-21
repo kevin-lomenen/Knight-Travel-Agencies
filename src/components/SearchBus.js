b37f81d2a2412fc5f278c6824947ec169b8e3f01
kevin-lomenen <kevin.lomenen@student.moringaschool.com>
2023-01-21 19:29:16 +0300

    Loading payment

--- src/components/SearchBus.js
+++ src/components/SearchBus.js
@@ -1,5 +1,5 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable array-callback-return */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client'
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from "./reportWebVitals";
 import "./style.scss";
-// import store from "./app/store";
-// import { Provider } from "react-redux";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <BrowserRouter>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
     </BrowserRouter>,
-  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
